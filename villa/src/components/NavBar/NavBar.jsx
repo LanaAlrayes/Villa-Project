c963b262445df9486a2bb2d751b7c1f28cb30281
LanaAlrayes <96716712+LanaAlrayes@users.noreply.github.com>
2024-03-13 21:33:49 +0300

    add component (NavBar)

--- villa/src/components/NavBar/NavBar.jsx
+++ villa/src/components/NavBar/NavBar.jsx
@@ -1,31 +1,54 @@
-import Container from 'react-bootstrap/Container';
-import Nav from 'react-bootstrap/Nav';
-import Navbar from 'react-bootstrap/Navbar';
-import Button from 'react-bootstrap/Button';
+import Container from "react-bootstrap/Container";
+import Nav from "react-bootstrap/Nav";
+import Navbar from "react-bootstrap/Navbar";
+import Button from "react-bootstrap/Button";
+// import { IoMdMail } from "react-icons/io";
 // import NavDropdown from 'react-bootstrap/NavDropdown';
 // import TopNavBar from "src/components/TopNavBar/TopNavBar.jsx"
 
-
-
 function NavBar() {
+  
   return (
     <>
-    {/* <TopNavBar /> */}
-    <Navbar expand="lg">
-      <Container style={{ height: '5rem' }}>
-        <Navbar.Brand href="#">VILLA</Navbar.Brand>
-        <Navbar.Toggle />
-        <Navbar.Collapse className="justify-content-center">
-          <Nav className="">
-            <Nav.Link href="#">Home</Nav.Link>
-            <Nav.Link href="#">Properties</Nav.Link>
-            <Nav.Link href="#">Property Details</Nav.Link>
-            <Nav.Link href="#">Contact Us</Nav.Link>
-          </Nav>
-        </Navbar.Collapse>
-        <Button variant="primary">Schedule a visit</Button>{' '}
-      </Container>
-    </Navbar>
+      {/* <TopNavBar /> */}
+      <Navbar expand="lg" className="m-2">
+        <Container>
+          <Navbar.Brand href="#" className="text-uppercase fs-3 fw-bold">
+            villa
+          </Navbar.Brand>
+          <Navbar.Toggle />
+          <Navbar.Collapse className="justify-content-end mx-4">
+            <Nav>
+              <Nav.Link className="mx-4" href="#">
+                Home
+              </Nav.Link>
+              <Nav.Link className="mx-4" href="#">
+                Properties
+              </Nav.Link>
+              <Nav.Link className="mx-4" href="#">
+                Property Details
+              </Nav.Link>
+              <Nav.Link className="mx-4" href="#">
+                Contact Us
+              </Nav.Link>
+            </Nav>
+          </Navbar.Collapse>
+          <Button
+            variant="dark"
+            className="mt-2"
+            style={{ borderRadius: "50px" }}
+          >
+             {/* <Button
+              variant="danger"
+              className="justify-content-start"
+              style={{ borderRadius: "50px" }}
+            >
+              <IoMdMail />
+            </Button>{" "} */}
+            Schedule a visit
+          </Button>{" "}
+        </Container>
+      </Navbar>
     </>
   );
 }
