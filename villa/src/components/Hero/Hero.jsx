4ea3c924fd80c992844cc10f0275593ab490b7d0
LanaAlrayes <96716712+LanaAlrayes@users.noreply.github.com>
2024-03-13 22:08:14 +0300

    add component (Hero) and photo

---
+++ villa/src/components/Hero/Hero.jsx
@@ -0,0 +1,76 @@
+import Carousel from "react-bootstrap/Carousel";
+import Image from "react-bootstrap/Image";
+import Row from "react-bootstrap/Row";
+import Col from "react-bootstrap/Col";
+
+function Hero() {
+  return (
+    <Carousel data-bs-theme="dark">
+      <Carousel.Item>
+        <Image
+          className="d-block w-100"
+          src="./banner-01.jpg"
+          alt="First slide"
+        />
+        <Carousel.Caption>
+          <Row className=" justify-content-start">
+            <Col className="text-start bg-danger " md="auto">
+              {" "}
+              <span className="bg-light" >
+                Toronto,<span className="text-danger"> Canada</span>
+              </span>
+              <h2 className=" fs-1 fw-bold text-light text-uppercase">
+                Hurry!
+                <br /> Get the Best <br /> Villa for <br /> you
+              </h2>
+            </Col>
+          </Row>
+        </Carousel.Caption>
+      </Carousel.Item>
+       <Carousel.Item>
+        <img
+          className="d-block w-100"
+          src="./banner-02.jpg"
+          alt="Second slide"
+        />
+       <Carousel.Caption>
+          <Row className=" justify-content-start">
+            <Col className="text-start bg-danger " md="auto">
+              {" "}
+              <span className="bg-light" >
+                Toronto,<span className="text-danger"> Canada</span>
+              </span>
+              <h2 className=" fs-1 fw-bold text-light text-uppercase">
+                Hurry!
+                <br /> Get the Best <br /> Villa for <br /> you
+              </h2>
+            </Col>
+          </Row>
+        </Carousel.Caption>
+      </Carousel.Item>
+      <Carousel.Item>
+        <img
+          className="d-block w-100"
+          src="./banner-03.jpg"
+          alt="Third slide"
+        />
+       <Carousel.Caption>
+          <Row className=" justify-content-start">
+            <Col className="text-start bg-danger " md="auto">
+              {" "}
+              <span className="bg-light" >
+                Toronto,<span className="text-danger"> Canada</span>
+              </span>
+              <h2 className=" fs-1 fw-bold text-light text-uppercase">
+                Hurry!
+                <br /> Get the Best <br /> Villa for <br /> you
+              </h2>
+            </Col>
+          </Row>
+        </Carousel.Caption>
+      </Carousel.Item>
+    </Carousel>
+  );
+}
+
+export default Hero;
