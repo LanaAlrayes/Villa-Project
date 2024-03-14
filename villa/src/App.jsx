13204813b85f17151ed0663e5738c474371fe1ff
LanaAlrayes <96716712+LanaAlrayes@users.noreply.github.com>
2024-03-14 14:34:07 +0300

    import (Featured & View)

--- villa/src/App.jsx
+++ villa/src/App.jsx
@@ -2,6 +2,9 @@ import "./App.css";
 import NavBar from "./components/NavBar/NavBar";
 import TopNavBar from "./components/TopNavBar/TopNavBar";
 import Hero from "./components/Hero/Hero";
+import Featured from "./components/Featured/Featured";
+import View from "./components/View/View";
+
 function App() {
   return (
     <div className="app">
@@ -9,8 +12,14 @@ function App() {
         email="info@company.com"
         location="Sunny Isles Beach, FL 33160"
       />
-      <NavBar />
+      <NavBar title="villa" />
       <Hero />
+      <Featured
+        title="Featured"
+        decs="Best Appartment"
+        decs1="& Sea View"
+      />
+      <View />
     </div>
   );
 }
