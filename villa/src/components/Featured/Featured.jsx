c39ec22944e4d0673bf4f28828dd1366287e189e
LanaAlrayes <96716712+LanaAlrayes@users.noreply.github.com>
2024-03-16 15:41:06 +0300

    edit name card & add pill

--- villa/src/components/Featured/Featured.jsx
+++ villa/src/components/Featured/Featured.jsx
@@ -1,36 +1,32 @@
 /* eslint-disable react/prop-types */
-import CardView from "../CardView/CardView";
+import CardFeatured from "../CardFeatured/CardFeatured";
+import Question from "../Question/Question";
 
 import Container from "react-bootstrap/Container";
 import Row from "react-bootstrap/Row";
 import Col from "react-bootstrap/Col";
 import Image from "react-bootstrap/Image";
 import Badge from "react-bootstrap/Badge";
-import Question from "../Question/Question";
 
 function Featured({ title, decs, decs1 }) {
   return (
-    <Container>
+    <Container expand="lg">
       <Row className="my-5">
-
         <Col xs={4}>
           <Image src="featured.jpg" />
           <Badge
-            className="bg-danger w-25 z-2 translate-middle"
-            style={{ borderRadius: "50%", height: "100px" }}
+            pill
+            className="bg-danger w-25 translate-middle"
+            style={{height: "100px"}}
           >
             <Image className="pt-3" src="featured-icon.png" />
           </Badge>
         </Col>
 
         <Col xs={5}>
-          <din className="text-danger fw-bolder text-uppercase d-inline-flex">
-            <span
-              className="vr mx-2"
-              style={{ width: "3px", height: "20px" }}
-            />
+          <div className="text-danger fw-bolder text-uppercase">
             <span>{title}</span>
-          </din>
+          </div>
           <h1 className="my-5 fw-bold">
             {decs} <br /> {decs1}
           </h1>
@@ -39,18 +35,19 @@ function Featured({ title, decs, decs1 }) {
             question2="How does this work ?"
             question3="Why is Villa Agency the best ?"
           />
-
         </Col>
-        <CardView
-          title1="250 m2"
-          sub="Total Flat Space"
-          title2="Contract"
-          sub1="Contract Ready"
-          title3="Payment"
-          sub2="Payment Process"
-          title4="Safety"
-          sub3=" 24/7 Under control"
-        />
+        <Col xs={3}>  
+          <CardFeatured
+            title1="250 m2"
+            sub="Total Flat Space"
+            title2="Contract"
+            sub1="Contract Ready"
+            title3="Payment"
+            sub2="Payment Process"
+            title4="Safety"
+            sub3=" 24/7 Under control"
+          />
+        </Col>
       </Row>
     </Container>
   );
