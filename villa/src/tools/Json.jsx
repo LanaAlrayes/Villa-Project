8924d51d36d7c8c7ceedfb153a3c560204ab3392
LanaAlrayes <96716712+LanaAlrayes@users.noreply.github.com>
2024-03-16 11:54:12 +0300

    add file Json (IconData & NavData )

---
+++ villa/src/tools/Json.jsx
@@ -0,0 +1,45 @@
+import { FaFacebook, FaLinkedin, FaTwitter, FaInstagram } from "react-icons/fa";
+
+export const IconData = [
+  { icon: <FaFacebook /> },
+  { icon: <FaTwitter /> },
+  { icon: <FaLinkedin /> },
+  { icon: <FaInstagram /> },
+];
+
+export const NavData = [
+  { desc: "Home" },
+  { desc: "Properties" },
+  { desc: "Property Details" },
+  { desc: "Contact Us" },
+];
+
+export const Json = [
+  {
+    img: "public/banner-01.jpg",
+    text: "Toronto, ",
+    text1: "Canada",
+    desc: "Hurry!",
+    desc1: "Get the Best",
+    desc2: "Villa for",
+    desc3: "you",
+  },
+  {
+    img: "public/banner-02.jpg",
+    text: "Melbourne, ",
+    text1: "Australia",
+    desc: "Be Quick!",
+    desc1: "Get the best",
+    desc2: "villa in",
+    desc3: "town",
+  },
+  {
+    img: "public/banner-03.jpg",
+    text: "Miami, ",
+    text1: "South Florida",
+    desc: "Act Now!",
+    desc1: "Get the",
+    desc2: "highest level",
+    desc3: "penthouse",
+  },
+];
